ec8da87b3244417f3cd28cbe4a55c2a24b4a8c51
Vuli Russell <vulirussell@gmail.com>
2020-09-24 17:34:08 +0100

    added 3d styling and rotation

--- main.js
+++ main.js
@@ -337,12 +337,40 @@ document.addEventListener('keydown', e => {
         case 'input button':
             inputButtonEvent(e.key,null);
             break;
+        case 'a':
+        case 'ArrowLeft':
+            yRotateInput -= 10;
+            updateRotation();
+            break;
+        case 'd':
+        case 'ArrowRight':
+            yRotateInput += 10;
+            updateRotation();
+            break;
+        case 'w':
+        case 'ArrowUp':
+            xRotateInput += 10;
+            updateRotation();
+            break;
+        case 's':
+        case 'ArrowDown':
+            xRotateInput -= 10;
+            updateRotation();
+            break;
+        case 'q':
+            zRotateInput -= 10;
+            updateRotation();
+            break;
+        case 'e':
+            zRotateInput += 10;
+            updateRotation();
+            break;
         default:
             break;
     }
 });
 
-document.addEventListener('click',e=>{console.log(e.target)});
+// document.addEventListener('click',e=>{console.log(e.target)});
 
 //TO ADD
 ////DONE after pressing =, if the next input is a digit/ans then the main display should be cleared before adding, rather than just appending the digit to the answer.
@@ -350,3 +378,26 @@ document.addEventListener('click',e=>{console.log(e.target)});
 //add arrows to navigate through input 
 ////DONE ans isnt working properly
 //rewrite event listeners to be a singel event listener and respond appropriately based on e.target.innerHTML or similar.
+//Two decimal placs in a row breaks it
+//standard form causes issuses (js return this from some calcs),  add step to parser
+//max number of characters??
+//add mouse interaction for rotation
+//m+ should only be able to be added when newexpression = true
+
+//DISPLAY STUFF
+const calculator = document.querySelector('.calculator')
+
+const updateRotation = () => {
+    transformStr = `rotateX(${xRotateInput}deg) rotateY(${yRotateInput}deg) rotateZ(${zRotateInput}deg`;
+    calculator.style.transform = transformStr;
+};
+
+//initial values
+let xRotateInput = 40;
+let yRotateInput = 20;
+let zRotateInput = -20;
+updateRotation();
+
+
+
+
